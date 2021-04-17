f442d7f36a7cf79f15ca49bff1ef303376efbb84
arfaz26 <chougulearfaz@gmail.com>
2021-04-17 20:23:57 +0530

    router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" exact component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
