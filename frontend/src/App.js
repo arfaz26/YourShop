9c67afbe1125e979c7ff974f04c6ac4fe9b2a8fc
arfaz26 <chougulearfaz@gmail.com>
2021-04-17 17:13:22 +0530

    header implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
-function App() {
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const App = () => {
   return (
     <>
-      <h1>WWelcome to yourshop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to yourshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
