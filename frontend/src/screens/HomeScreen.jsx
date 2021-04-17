774be5787161f2084fb038f779d1ec532c6d8854
arfaz26 <chougulearfaz@gmail.com>
2021-04-17 20:05:01 +0530

    ratings component implemented

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((prod) => (
-          <Col sm={3} md={6} lg={4} xl={3}>
+          <Col key={prod._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={prod} />
           </Col>
         ))}
