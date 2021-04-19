786e1e81d126ba73f5046c6a107b82f9d54bc29a
arfaz26 <chougulearfaz@gmail.com>
2021-04-19 14:34:34 +0530

    api routes seperated

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const color = require("colors");
-const products = require("./data/products");
 const connect = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
-
 dotenv.config();
 
 connect();
@@ -14,14 +13,7 @@ app.get("/", (req, res) => {
   res.send("api is running.......");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
