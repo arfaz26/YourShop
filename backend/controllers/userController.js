517360cf65fd7de80644cac62e9eda7db7cc394b
arfaz26 <chougulearfaz@gmail.com>
2021-04-21 20:35:01 +0530

    update user profile endpoint created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,3 +66,29 @@ exports.getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
+
+exports.updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    const token = generateToken(updatedUser._id);
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: token,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
