fc611dc77633183029c8cf08d275599bfdb048d0
arfaz26 <chougulearfaz@gmail.com>
2021-04-21 15:14:55 +0530

    signup implemeneted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,36 @@ exports.authUser = asyncHandler(async (req, res) => {
   }
 });
 
+exports.registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists with that email");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const token = generateToken(user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 exports.getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
+router.route("/").post(userController.registerUser);
 router.post("/login", userController.authUser);
 router.route("/profile").get(protect, userController.getUserProfile);
 
