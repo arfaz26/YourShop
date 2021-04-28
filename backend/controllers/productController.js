a5f2fa33167f4aa74fd78c7f4376f4e564751823
arfaz26 <chougulearfaz@gmail.com>
2021-04-28 12:09:04 +0530

    delete product route added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,3 +15,16 @@ exports.getProductById = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+exports.deleteroductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({
+      message: "product removed",
+    });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const postController = require("../controllers/productController");
+const protectedRoutes = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").get(postController.getProducts);
 
-router.route("/:id").get(postController.getProductById);
+router
+  .route("/:id")
+  .get(postController.getProductById)
+  .delete(
+    protectedRoutes.protect,
+    protectedRoutes.isAdmin,
+    postController.deleteroductById
+  );
 
 module.exports = router;
