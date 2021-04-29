0353b322f2ee94762514dbdd5b9322fd9d5c410e
arfaz26 <chougulearfaz@gmail.com>
2021-04-29 12:34:37 +0530

    image upload route created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const color = require("colors");
 const connect = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -20,9 +22,13 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 
